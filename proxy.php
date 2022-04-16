6fdb2b8c199b80fa539988b55d87968a607bc92c
Nicsoart <Nicso.art@yandex.com>
2022-04-16 21:50:19 +0900

    Update proxy.php

--- proxy.php
+++ proxy.php
@@ -28,7 +28,7 @@ header($header);
  
 // you only need the following headers if the proxy and WebGL are hosted on different domains
 header("Access-Control-Allow-Credentials: true");
-header("Access-Control-Allow-Headers: Accept, X-Access-Token, X-Application-Name, X-Request-Sent-Time, X-Requested-With");
+header("Access-Control-Allow-Headers: Accept, X-Access-Token, X-Application-Name, X-Request-Sent-Time, X-Requested-With, XMLHttpRequest");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Origin: *");
 // you can replace the "*" above with "http://your-webgl-domain" to prevent unauthorized use of your proxy
