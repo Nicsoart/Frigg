4cccd13ad411ae376ff35e8c66b3d499395e22b4
Nicsoart <Nicso.art@yandex.com>
2022-04-17 02:07:31 +0900

    build10

--- proxy.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-header("Access-Control-Allow-Credentials: true");
-header("Access-Control-Allow-Headers: Accept, X-Access-Token, X-Application-Name, X-Request-Sent-Time");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Origin: https://frigganalyzer.ru");
-
-echo $_GET["url"];
